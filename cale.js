0704106d7b9f8ee94e8a1f58cd4f7ad441774051
abigailmtz <65188586+abigailmtz@users.noreply.github.com>
2020-05-11 21:12:35 -0500

    Update cale.js

--- cale.js
+++ cale.js
@@ -1,6 +1,38 @@
-var winHeight = $(window).height();
-var height = ( winHeight * 16.6666 ) / 100;
-var lineHeight = height + "px";
+var app = {
+	settings: {
+		container: $('.calendar'),
+		calendar: $('.front'),
+		days: $('.weeks span'),
+		form: $('.back'),
+		input: $('.back input'),
+		buttons: $('.back button')
+	},
 
-$("li").css("line-height", lineHeight);
-$("li").css("height", height);
+	init: function() {
+		instance = this;
+		settings = this.settings;
+		this.bindUIActions();
+	},
+
+	swap: function(currentSide, desiredSide) {
+		settings.container.toggleClass('flip');
+
+		currentSide.fadeOut(900);
+		currentSide.hide();
+
+		desiredSide.show();
+	},
+
+	bindUIActions: function() {
+		settings.days.on('click', function(){
+			instance.swap(settings.calendar, settings.form);
+			settings.input.focus();
+		});
+
+		settings.buttons.on('click', function(){
+			instance.swap(settings.form, settings.calendar);
+		});
+	}
+}
+
+app.init();
